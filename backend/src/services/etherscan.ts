01ac5bb33ba3464d73a30fcd310d9ec5e99c4f9c
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-20 18:04:53 +0530

    feat(etherscan): support proxy metadata and implementation resolution

--- backend/src/services/etherscan.ts
+++ backend/src/services/etherscan.ts
@@ -4,10 +4,9 @@ const BASE_URL = "https://api.etherscan.io/v2/api";
 
 export async function fetchContractFromEtherscan(address:string) {
     try {
-        console.log("hello00");
         const response = await axios.get(BASE_URL, {
             params: {
-                chainid: 11155111,
+                chainid: 1,
                 module: "contract",
                 action: "getsourcecode",
                 address,
@@ -15,27 +14,18 @@ export async function fetchContractFromEtherscan(address:string) {
             }
         });
 
-        console.log("hello011");
-
         const result = response.data.result?.[0];
-        console.log("result: ",result.data);
-        console.log("result: ",result.ContractName);
-        console.log("result: ",result);
-
-        console.log("hello012");
 
         if(!result || !result.SourceCode) {
             console.log("no source code");
             return null;
         }
 
-        console.log("hello013");
-
         if (result.ABI === "Contract source code not verified") {
             return null;
         }
 
-        console.log("hello0114");
+        console.log("Implemetations: ", result.Implementation);
 
         return {
             contractName: result.ContractName,
@@ -43,6 +33,7 @@ export async function fetchContractFromEtherscan(address:string) {
             sourceCode: result.SourceCode,
             compilerVersion: result.CompilerVersion,
             proxy: result.Proxy === "1",
+            implementations: result.Implementation || null
         };
 
     }catch(e) {
