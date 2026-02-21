82383f2c096282cd65720ea5028e862f45dcb4c2
Priyanshu Singh Chouhan <janneker9@gmail.com>
2026-02-21 20:50:53 +0530

    feat: add a check if api key is available

--- backend/src/services/etherscan.ts
+++ backend/src/services/etherscan.ts
@@ -2,42 +2,44 @@ import axios from "axios";
 
 const BASE_URL = "https://api.etherscan.io/v2/api";
 
-export async function fetchContractFromEtherscan(address:string) {
-    try {
-        const response = await axios.get(BASE_URL, {
-            params: {
-                chainid: 1,
-                module: "contract",
-                action: "getsourcecode",
-                address,
-                apikey: process.env.ETHERSCAN_API_KEY,
-            }
-        });
-
-        const result = response.data.result?.[0];
-
-        if(!result || !result.SourceCode) {
-            console.log("no source code");
-            return null;
-        }
-
-        if (result.ABI === "Contract source code not verified") {
-            return null;
-        }
-
-        console.log("Implemetations: ", result.Implementation);
-
-        return {
-            contractName: result.ContractName,
-            abi: JSON.parse(result.ABI),
-            sourceCode: result.SourceCode,
-            compilerVersion: result.CompilerVersion,
-            proxy: result.Proxy === "1",
-            implementations: result.Implementation || null
-        };
-
-    }catch(e) {
-        console.log("Etherscan fetch error: ", e);
-        throw new Error("Failed to fetch contract");
+export async function fetchContractFromEtherscan(address: string) {
+  try {
+    if (!process.env.ETHERSCAN_API_KEY) {
+      throw new Error("ETHERSCAN_API_KEY is required");
     }
+    const response = await axios.get(BASE_URL, {
+      params: {
+        chainid: 1,
+        module: "contract",
+        action: "getsourcecode",
+        address,
+        apikey: process.env.ETHERSCAN_API_KEY,
+      },
+    });
+
+    const result = response.data.result?.[0];
+
+    if (!result || !result.SourceCode) {
+      console.log("no source code");
+      return null;
+    }
+
+    if (result.ABI === "Contract source code not verified") {
+      return null;
+    }
+
+    console.log("Implemetations: ", result.Implementation);
+
+    return {
+      contractName: result.ContractName,
+      abi: JSON.parse(result.ABI),
+      sourceCode: result.SourceCode,
+      compilerVersion: result.CompilerVersion,
+      proxy: result.Proxy === "1",
+      implementations: result.Implementation || null,
+    };
+  } catch (e) {
+    console.log("Etherscan fetch error: ", e);
+    throw new Error("Failed to fetch contract");
+  }
 }
